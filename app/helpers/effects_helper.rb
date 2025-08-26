e0631e3e3affa2d1ae931775670f24fbde00175e
agustinlopez9 <fernandopiceda99@gmail.com>
2025-08-26 20:53:19 -0300

    add slide-up effect

--- app/helpers/effects_helper.rb
+++ app/helpers/effects_helper.rb
@@ -12,9 +12,9 @@ module EffectsHelper
   end
 
   def example_layout_partial(title = "")
-    content_tag :div, class: "py-20" do
+    content_tag :div, class: "py-4 my-4" do
       content = content_tag :h3, title, class: "font-bold text-4xl"
-      content += content_tag :div, class: "flex flex-row flex-wrap items-start justify-between gap-4 my-20" do
+      content += content_tag :div, class: "flex flex-row flex-wrap items-start justify-between gap-4 h-full my-20 pb-20" do
         yield
       end
     end
---
+++ app/javascript/controllers/slide_up_controller.js
@@ -0,0 +1,79 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["element"];
+  static values = {
+    duration: { type: Number, default: 500 },
+    delay: { type: Number, default: 0 },
+    trigger: { type: String, default: "connect" },
+  };
+
+  previousIntersectionRatio = 0;
+
+  connect() {
+    this.setupInitialState();
+
+    // Animate based on trigger type
+    if (this.triggerValue === "connect") {
+      this.startSlideUp();
+    } else if (this.triggerValue === "scroll") {
+      this.setupScrollObserver();
+    }
+  }
+
+  setupInitialState() {
+    const childElement = this.element.children[0];
+    childElement.style.opacity = "0";
+    childElement.style.transform = "translateY(10%)";
+    childElement.style.transition = `opacity ${this.durationValue}ms ease-out, transform ${this.durationValue}ms ease-out`;
+  }
+
+  startSlideUp() {
+    const childElement = this.element.children[0];
+    setTimeout(() => {
+      childElement.style.opacity = "1";
+      childElement.style.transform = "translateY(0)";
+    }, this.delayValue);
+  }
+
+  handleElementIntersect = (entry) => {
+    if (
+      entry.isIntersecting &&
+      entry.intersectionRatio > this.previousIntersectionRatio
+    ) {
+      this.startSlideUp();
+    } else if (
+      entry.boundingClientRect.y >= 0 &&
+      entry.intersectionRatio < this.previousIntersectionRatio
+    ) {
+      this.setupInitialState();
+    }
+    this.previousIntersectionRatio = entry.intersectionRatio;
+  };
+
+  setupScrollObserver() {
+    this.intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          this.handleElementIntersect(entry);
+        });
+      },
+      {
+        threshold: 0.5,
+        rootMargin: "0px 0px -50px 0px",
+      }
+    );
+
+    this.intersectionObserver.observe(this.element);
+  }
+
+  stopIntersectionObserver() {
+    if (this.intersectionObserver) {
+      this.intersectionObserver.unobserve(this.element);
+    }
+  }
+
+  disconnect() {
+    this.stopIntersectionObserver();
+  }
+}
